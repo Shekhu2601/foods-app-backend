645f47e3dbb1d6e4f5b7b8fa128c8fa26b6c68e5
Shekhu26001 <shekhar99779977@gmail.com>
2024-11-25 09:08:24 +0530

    signup and login page

---
+++ index.js
@@ -0,0 +1,20 @@
+const express =require("express")
+const app = express();
+const dotenv= require("dotenv");
+const bodyParser = require("body-parser");
+const cors= require("cors")
+const AuthRouter =require("./Routes/Authrouter")
+const ProductRouter =require("./Routes/Products")
+dotenv.config();
+require("./Models/db")
+app.get("/d",(req,res)=>{
+    res.send("ram")
+})
+app.use(bodyParser.json())
+app.use(cors())
+const PORT =process.env.PORT
+app.use('/auth', AuthRouter);
+app.use('/auth', ProductRouter);
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT} `)
+})
